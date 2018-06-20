78f4a1feeaf8a088f8220f5e8fb54d37ff7606a4
btfpost <btfpost@gmail.com>
2018-06-20 10:18:55 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,4 +3,4 @@ var Data1= '{"first":"Anna", "last":"Chernova", "city":"Cincinnati"}';
 var Data = JSON.parse(Data1);
 console.log(Data);
 
-document.getElementById("message").innerHTML=Data.first;
+document.getElementById('message').innerHTML=Data.first;
